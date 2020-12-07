24c0226368e29993a1d3649936d27dcaa3bc0634
Akbeta <34417875+Akbeta@users.noreply.github.com>
2020-12-07 22:29:30 +0300

    delete unnecessary codes

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,18 +1,13 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <PostComponent />
+    <p>deneme</p>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import PostComponent from "@/components/PostComponent.vue";
 
 export default {
   name: "Home",
-  components: {
-    PostComponent
-  }
+  components: {}
 };
 </script>
