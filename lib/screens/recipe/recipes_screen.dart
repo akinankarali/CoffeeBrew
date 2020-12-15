a4daba0e511ab8e10d1a80adfe0385e2c620d00d
Akbeta <34417875+Akbeta@users.noreply.github.com>
2020-12-16 02:00:43 +0300

    create recipe screen

---
+++ lib/screens/recipe/recipes_screen.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+import '../../constants.dart';
+
+class RecipesScreen extends StatelessWidget {
+  const RecipesScreen({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: kPrimaryColor,
+      appBar: buildAppBar(),
+    );
+  }
+
+  AppBar buildAppBar() {
+    return AppBar(
+      elevation: 0,
+      title: Text('Select Your Recipe'),
+      centerTitle: false,
+      actions: [
+        IconButton(
+          icon: SvgPicture.asset(
+            "assets/icons/add.svg",
+            color: Colors.white,
+            width: 18,
+            height: 18,
+          ),
+          onPressed: () => {},
+        )
+      ],
+    );
+  }
+}
