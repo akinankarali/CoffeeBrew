c697691e237ebdb211ab59ab85bf6855f9be5d05
Akbeta <34417875+Akbeta@users.noreply.github.com>
2020-12-16 01:59:19 +0300

    add global variables

---
+++ lib/constants.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+const kBackgroundColor = Color(0xff3c3f51);
+const kPrimaryColor = Color(0xff282a34);
+const kSecondaryColor = Color(0xffeff0d1);
+const kTextColor = Color(0xff262730);
+const kTextLightColor = Color(0xff3a3b43);
+const kYellowColor = Color(0xfffcac46);
+const kYellowDarkColor = Color(0xffb36500);
+
+const kDefaultPadding = 20.0;
+
+const kDefaultShadow = BoxShadow(
+  offset: Offset(0, 15),
+  blurRadius: 27,
+  color: Colors.black12,
+);
