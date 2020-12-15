dd93991d62fec082cd35aea76f0841fad3614f71
Akbeta <34417875+Akbeta@users.noreply.github.com>
2020-12-15 22:25:22 +0300

    add colors and fonts

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,40 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(MyApp());
 }
 
+Map<int, Color> color = {
+  50: Color.fromRGBO(40, 42, 52, .1),
+  100: Color.fromRGBO(40, 42, 52, .2),
+  200: Color.fromRGBO(40, 42, 52, .3),
+  300: Color.fromRGBO(40, 42, 52, .4),
+  400: Color.fromRGBO(40, 42, 52, .5),
+  500: Color.fromRGBO(40, 42, 52, .6),
+  600: Color.fromRGBO(40, 42, 52, .7),
+  700: Color.fromRGBO(40, 42, 52, .8),
+  800: Color.fromRGBO(40, 42, 52, .9),
+  900: Color.fromRGBO(40, 42, 52, 1),
+};
+
+MaterialColor colorCustom = MaterialColor(0xFF282a34, color);
+Color darkerYellow = const Color(0xffb36500);
+Color lightYellow = const Color(0xfffcac46);
+Color bodyColor = const Color(0xff3c3f51);
+
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: colorCustom,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(title: 'Navorski'),
     );
   }
 }
@@ -28,39 +49,31 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+        title: Text(
+          widget.title,
+          style: TextStyle(
+            color: lightYellow,
+            fontSize: 26,
+            fontFamily: 'Inter',
+            fontWeight: FontWeight.w700,
+          ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
+      body: Center(
+        child: Text(
+          'deneme',
+          style: TextStyle(
+            fontFamily: 'Inter',
+            fontSize: 24,
+            color: Colors.white,
+          ),
+        ),
       ),
+      backgroundColor: bodyColor,
     );
   }
 }
