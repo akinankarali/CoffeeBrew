9ae84c659261b9f1de0687c2470f9056fce1f1ec
Akbeta <34417875+Akbeta@users.noreply.github.com>
2020-12-16 02:00:11 +0300

    delete variables and add structure

--- lib/main.dart
+++ lib/main.dart
@@ -1,79 +1,25 @@
-import 'dart:ui';
-
+import 'package:coffee_brew/constants.dart';
+import 'package:coffee_brew/screens/recipe/recipes_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(MyApp());
 }
 
-Map<int, Color> color = {
-  50: Color.fromRGBO(40, 42, 52, .1),
-  100: Color.fromRGBO(40, 42, 52, .2),
-  200: Color.fromRGBO(40, 42, 52, .3),
-  300: Color.fromRGBO(40, 42, 52, .4),
-  400: Color.fromRGBO(40, 42, 52, .5),
-  500: Color.fromRGBO(40, 42, 52, .6),
-  600: Color.fromRGBO(40, 42, 52, .7),
-  700: Color.fromRGBO(40, 42, 52, .8),
-  800: Color.fromRGBO(40, 42, 52, .9),
-  900: Color.fromRGBO(40, 42, 52, 1),
-};
-
-MaterialColor colorCustom = MaterialColor(0xFF282a34, color);
-Color darkerYellow = const Color(0xffb36500);
-Color lightYellow = const Color(0xfffcac46);
-Color bodyColor = const Color(0xff3c3f51);
-
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Coffee Brew App',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        primarySwatch: colorCustom,
+        textTheme: GoogleFonts.interTextTheme(Theme.of(context).textTheme),
+        primaryColor: kPrimaryColor,
+        accentColor: kPrimaryColor,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: MyHomePage(title: 'Navorski'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(
-          widget.title,
-          style: TextStyle(
-            color: lightYellow,
-            fontSize: 26,
-            fontFamily: 'Inter',
-            fontWeight: FontWeight.w700,
-          ),
-        ),
-      ),
-      body: Center(
-        child: Text(
-          'deneme',
-          style: TextStyle(
-            fontFamily: 'Inter',
-            fontSize: 24,
-            color: Colors.white,
-          ),
-        ),
-      ),
-      backgroundColor: bodyColor,
+      home: RecipesScreen(),
     );
   }
 }
